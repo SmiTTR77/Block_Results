cbfc5308b9ab4cf4849b087e82df2d072ed45b65
Vladimir Smiryagin <smitt.r77@yandex.ru>
2023-03-21 16:46:46 +0300

    ControlWork complete

--- FinalControlWork1/Program.cs
+++ FinalControlWork1/Program.cs
@@ -10,3 +10,50 @@ Task: Написать программу, которая из имеющего�
 ["Russia", "Denmark", "Kazan"] -> []
 */
 
+string[] CreateArray(int size)
+{
+	string[] array = new string[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		Console.Write("Input a something: ");
+		array[i] = Console.ReadLine();
+	}
+	return array;
+}
+
+void ShowArray(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+		Console.Write(array[i] + " ");
+	Console.WriteLine();
+}
+
+string[] MaxThreeChar(string[] array)
+{
+	int j = 0;
+	string[] tempArray = new string[array.Length];
+
+	for (int i = 0; i < array.Length; i++)
+		if (array[i].Length <= 3)
+		{
+			tempArray[j] = array[i];
+			j++;
+		}
+
+	string[] modifiedArray = new string[j];
+	for (int i = 0; i < j; i++)
+		modifiedArray[i] = tempArray[i];
+	return modifiedArray;
+}
+
+Console.Write("Input a size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] newArray = CreateArray(size);
+Console.WriteLine("Original array: ");
+ShowArray(newArray);
+
+string[] updatedArray = MaxThreeChar(newArray);
+Console.WriteLine("Updated array:");
+ShowArray(updatedArray);
